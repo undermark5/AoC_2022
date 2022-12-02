11082b51e5cb46094d20c59915a15fcb346663ff
Nathan Shurtz <n.shurtz@fetchrewards.com>
2022-12-02 00:03:41 -0600

    day2 both stars

--- src/day02.jl
+++ src/day02.jl
@@ -5,14 +5,28 @@ include("utils.jl")
 example = readlines("./data/day02_example.txt")
 input = readlines("./data/day02.txt")
 
+win_table = [
+    [3, 0, 6],
+    [6, 3, 0],
+    [0, 6, 3]
+]
+
+win_table2 = [0, 3, 6]
+shape_table = [
+    [3, 1, 2],
+    [1, 2, 3],
+    [2, 3, 1]
+]
+
+
 function part_1(input)
-    nothing
+    map(it -> split(it) .|> first |> it -> (it[1] - 'A' + 1, it[2] - 'X' + 1) |> it -> (score = win_table[it[2]][it[1]] + it[2]), input) |> sum
 end
 @show part_1(example)
 @show part_1(input)
 
 function part_2(input)
-    nothing
+    map(it -> split(it) .|> first |> it -> (it[1] - 'A' + 1, it[2] - 'X' + 1) |> it -> (score = shape_table[it[2]][it[1]] + win_table2[it[2]]), input) |> sum
 end
 @show part_2(example)
 @show part_2(input)
