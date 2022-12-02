e6ddff209384ca54b1256a417a14a1effdb187ac
Nathan Shurtz <n.shurtz@fetchrewards.com>
2022-12-01 23:01:51 -0600

    start day02

--- src/day02.jl
+++ src/day02.jl
@@ -1,14 +1,18 @@
 # https://adventofcode.com/2022/day/2
 using AdventOfCodeUtil
+include("utils.jl")
 
+example = readlines("./data/day02_example.txt")
 input = readlines("./data/day02.txt")
 
 function part_1(input)
     nothing
 end
-@info part_1(input)
+@show part_1(example)
+@show part_1(input)
 
 function part_2(input)
     nothing
 end
-@info part_2(input)
+@show part_2(example)
+@show part_2(input)
