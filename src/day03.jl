7a9419f35423fce284e09ea787357ed1b6832dc1
Nathan Shurtz <n.shurtz@fetchrewards.com>
2022-12-02 23:48:34 -0600

    day3 complete

--- src/day03.jl
+++ src/day03.jl
@@ -6,13 +6,33 @@ example = readlines("./data/day03_example.txt")
 input = readlines("./data/day03.txt")
 
 function part_1(input)
-    nothing
+    map(function(it)
+        size = Int(length(it) / 2)
+        item = intersect(first(it, size),last(it, size))[1][1]
+        priority = 0
+        if item in 'a':'z'
+            priority = item - 'a' + 1
+        elseif item in 'A':'Z'
+            priority = item - 'A' + 27
+        end
+        priority
+    end, input) |> sum
 end
 @show part_1(example)
 @show part_1(input)
 
 function part_2(input)
-    nothing
+    groups = Base.Iterators.partition(input, 3)
+    map(function(it)
+        item = intersect(it...)[1]
+        priority = 0
+        if item in 'a':'z'
+            priority = item - 'a' + 1
+        elseif item in 'A':'Z'
+            priority = item - 'A' + 27
+        end
+        priority
+    end, groups) |> sum
 end
 @show part_2(example)
 @show part_2(input)
