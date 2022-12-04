9155e30194525942230820be6da433616a300fd1
Nathan Shurtz <n.shurtz@fetchrewards.com>
2022-12-03 23:08:15 -0600

    start day04

---
+++ src/day04.jl
@@ -0,0 +1,18 @@
+# https://adventofcode.com/2022/day/4
+using AdventOfCodeUtil
+include("utils.jl")
+
+example = readlines("./data/day04_example.txt")
+input = readlines("./data/day04.txt")
+
+function part_1(input)
+    nothing
+end
+@show part_1(example)
+@show part_1(input)
+
+function part_2(input)
+    nothing
+end
+@show part_2(example)
+@show part_2(input)
