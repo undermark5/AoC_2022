905dfb7c8c702410feb1dfd61cd6dc2dce2f51a1
Nathan Shurtz <n.shurtz@fetchrewards.com>
2022-12-05 23:06:28 -0600

    start day06

---
+++ src/day06.jl
@@ -0,0 +1,18 @@
+# https://adventofcode.com/2022/day/6
+using AdventOfCodeUtil
+include("utils.jl")
+
+example = readlines("./data/day06_example.txt")
+input = readlines("./data/day06.txt")
+
+function part_1(input)
+    nothing
+end
+@show part_1(example)
+@show part_1(input)
+
+function part_2(input)
+    nothing
+end
+@show part_2(example)
+@show part_2(input)
